a42e2a15549be4955a1321ba31f26a1aea0fc06f
Tatyana729 <106909902+Tatyana729@users.noreply.github.com>
2022-06-21 23:02:27 +0300

    Update 09_task_tuple.py

--- Module3/practice/list_and_tuple/09_task_tuple.py
+++ Module3/practice/list_and_tuple/09_task_tuple.py
@@ -2,3 +2,15 @@
 # Найдите количество элементов, которые встречаются во всех трех кортежах.
 
 # TODO: your code here
+
+tuple1 = 1,2,3,4,5
+tuple2 = 1,6,7,8,9
+tuple3 = 10,11,12,9,8
+i = 0
+
+tuple_total = tuple3+tuple2+tuple1
+
+for el_tuple in tuple_total:
+    if tuple_total.count(el_tuple)>1:
+        i+=0.5
+print(int(i))
