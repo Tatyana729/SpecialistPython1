624fca1add509b06de6c487f9009ac8b30a6fac1
Tatyana729 <106909902+Tatyana729@users.noreply.github.com>
2022-07-03 15:29:38 +0300

    Update 01_task_struct.py

--- Module8/practice/structs/01_task_struct.py
+++ Module8/practice/structs/01_task_struct.py
@@ -1,3 +1,9 @@
 # Бегун проводил ежедневные тренировки и записывал расстояния которые пробегал за занятия в метрах:
 distances = [600, 755, 321.6, 1234, 231, 456.6, 4561, 1200, 456]
 # Переведите все значения в футы (получить и вывести новый список)
+fut_metr = 0.3048
+distances_in_fut =[]
+for el in distances:
+    new_el = el/fut_metr
+    distances_in_fut.append(new_el)
+print(distances_in_fut)
